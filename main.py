a1193a45ee0b08c7989a0f01cc4f274819701e6a
chloeeshook <156020147+chloeeshook@users.noreply.github.com>
2024-02-18 22:08:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+profit = 125.00
+salePrice = 243.75
+saleProfit = 43.75
+
+print("Item Name: " + itemName)
+print("Retail Price: $" + str(retailPrice))
+print("Wholesale Price: $" + str(wholesalePrice))
+print("Profit: $" + str(profit))
+print("Sale Price: $" + str(salePrice))
+print("Sale Profit: $" + str(saleProfit))
